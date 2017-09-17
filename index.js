81b382df31efb6f80df6253a1bca257bf0205f62
Raphael Pinto <raphaelcpinto@gmail.com>
2017-09-17 11:24:51 -0300

    Remove Arrow function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
   function getQuery (queryParams) {
     var arr = Object.keys(queryParams).map(function (k) {
       if(Array.isArray(queryParams[k])){
-        return queryParams[k].map((v) => {
+        return queryParams[k].map(function (v) {
           return [k+'[]', encodeURIComponent(v)].join('=')
         }).join("&")
       }
